bd02c2d7c5564b1b67b7dd96f3b4a98fdaf397bd
meki63 <144960473+meki63@users.noreply.github.com>
2024-10-08 20:29:31 -0400

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "my-terraform-state-bucket-devops-miklit"  # Replace with your S3 bucket name
+    bucket = "my-terraform-state-bucket-devops-meklit"  # Replace with your S3 bucket name
     key    = "qa/terraform.tfstate"  # Path to store the state file in the bucket
     region = "us-east-1"  # Region where your bucket is located
   }
